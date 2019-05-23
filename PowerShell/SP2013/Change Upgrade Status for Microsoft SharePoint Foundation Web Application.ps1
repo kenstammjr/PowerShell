635241ad78723386f05f52497c89d7478c848170
ken.stamm <ken.stamm@KES-SP13>
2019-05-23 09:00:41 -0700

    updates

---
+++ PowerShell/SP2013/Change Upgrade Status for Microsoft SharePoint Foundation Web Application.ps1
@@ -0,0 +1,10 @@
+Add-PSSnapin Microsoft.SharePoint.PowerShell
+
+$webappsvc = Get-SPServiceInstance -server $env:COMPUTERNAME | ? { $_.TypeName -eq "Microsoft SharePoint Foundation Web Application" }
+$instance = Get-SPServiceInstance $webappsvc.Id
+Write-Output "Status = $($instance.status)"
+Write-Output "NeedsUpgrade = $($instance.needsupgrade)"
+if ($instance.needsupgrade){
+    $instance.NeedsUpgrade = $false
+    Write-Output "NeedsUpgrade = $($instance.needsupgrade)"
+}
